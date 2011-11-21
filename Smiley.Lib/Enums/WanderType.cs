17f3b60272302752692e2f9adfdf223aaf6759cc
einterval <einterval@6d05c76d-9417-5cb7-c64e-46df74901b00>
2011-11-21 00:57:10 +0000

    3 enums (enemytype, projectiletype, wandertype)

---
+++ Smiley.Lib/Enums/WanderType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smiley.Lib.Enums
+{
+    public enum WanderType
+    {
+        WanderRandomly = 0,
+        WanderLeftRight = 1,
+        WanderUpDown = 2,
+        WanderStandStill = 3
+    }
+}
