10800c30d84e09d18feb86e1471ae75920c512ce
einterval <einterval@6d05c76d-9417-5cb7-c64e-46df74901b00>
2011-11-21 05:54:29 +0000

    Added incomplete shells of particle system files

---
+++ trunk/Smiley.Lib/Framework/ParticleSystemManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smiley.Lib.Framework
+{
+    public class ParticleSystemManager
+    {
+        /// <summary>
+        /// Constructs a new particle system manager.
+        /// </summary>
+        public ParticleSystemManager()
+        {
+            
+        }
+
+        public ~ParticleSystemManager()
+        {
+        
+        }
+
+        public void Update(float dt)
+        {
+        
+        }
+
+        //TODO: add to this.
+        
+    }
+}
