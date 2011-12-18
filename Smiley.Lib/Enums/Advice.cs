41c4a0476850c423b61e8f124cee7be730356b35
rgw0094 <rgw0094@6d05c76d-9417-5cb7-c64e-46df74901b00>
2011-12-18 18:56:31 +0000

    ported PopupMessageManager

---
+++ Smiley.Lib/Enums/Advice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smiley.Lib.Enums
+{
+    public enum Advice
+    {
+        Saving,
+        Inventory,
+        Mana,
+        Shop,
+        Frisbee,
+        Bomb,
+        Smilelet,
+        Exit
+    }
+}
